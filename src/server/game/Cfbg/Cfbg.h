602b87a80cf01c5288ce5b44f33942b555dd64a2
Wolord <omid_daei@ymail.com>
2014-08-25 00:02:42 +0430

    Crossfaction Battlegrounds Implementation

---
+++ src/server/game/Cfbg/Cfbg.h
@@ -0,0 +1,36 @@
+#ifndef _CUSTOM_H
+#define _CUSTOM_H
+
+#define MSG_COLOR_LIGHTRED     "|cffFF6060"
+#define MSG_COLOR_LIGHTBLUE    "|cff00CCFF"
+#define MSG_COLOR_ANN_GREEN    "|cff40AF20"
+#define MSG_COLOR_RED          "|cffFF0000"
+#define MSG_COLOR_GOLD         "|cffFFCC00"
+#define MSG_COLOR_SUBWHITE     "|cffBBBBBB"
+#define MSG_COLOR_MAGENTA      "|cffFF00FF"
+#define MSG_COLOR_YELLOW       "|cffFFFF00"
+#define MSG_COLOR_CYAN         "|cff00FFFF"
+#define MSG_COLOR_DARKBLUE     "|cff0000FF"
+
+#define MSG_COLOR_GREY         "|cff9D9D9D"
+#define MSG_COLOR_WHITE        "|cffFFFFFF"
+#define MSG_COLOR_GREEN        "|cff1EFF00"
+#define MSG_COLOR_BLUE         "|cff0080FF"
+#define MSG_COLOR_PURPLE       "|cffB048F8"
+#define MSG_COLOR_ORANGE       "|cffFF8000"
+
+#define MSG_COLOR_DRUID        "|cffFF7D0A"
+#define MSG_COLOR_HUNTER       "|cffABD473"
+#define MSG_COLOR_MAGE         "|cff69CCF0"
+#define MSG_COLOR_PALADIN      "|cffF58CBA"
+#define MSG_COLOR_PRIEST       "|cffFFFFFF"
+#define MSG_COLOR_ROGUE        "|cffFFF569"
+#define MSG_COLOR_SHAMAN       "|cff0070DE"
+#define MSG_COLOR_WARLOCK      "|cff9482C9"
+#define MSG_COLOR_WARRIOR      "|cffC79C6E"
+#define MSG_COLOR_DEATH_KNIGHT "|cffC41F3B"
+#define MSG_COLOR_MONK         "|cff00FF96"
+
+#define LIMIT_UINT32 2147483647
+
+#endif
