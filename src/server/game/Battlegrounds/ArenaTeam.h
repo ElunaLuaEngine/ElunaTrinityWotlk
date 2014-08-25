7594d0f652aa8d3e6d5ddf1186e1d0485fa73b9b
Wolord <omid_daei@ymail.com>
2014-08-25 12:32:29 +0430

    Arena 1v1 Implementation

--- src/server/game/Battlegrounds/ArenaTeam.h
+++ src/server/game/Battlegrounds/ArenaTeam.h
@@ -80,6 +80,7 @@ ERR_ARENA_TEAM_LEVEL_TOO_LOW_I
 
 enum ArenaTeamTypes
 {
+    ARENA_TEAM_1v1      = 1,
     ARENA_TEAM_2v2      = 2,
     ARENA_TEAM_3v3      = 3,
     ARENA_TEAM_5v5      = 5
@@ -111,7 +112,7 @@ struct ArenaTeamStats
     uint32 Rank;
 };
 
-#define MAX_ARENA_SLOT 3                                    // 0..2 slots
+#define MAX_ARENA_SLOT 4                                    // 0..3 slots
 
 class ArenaTeam
 {
