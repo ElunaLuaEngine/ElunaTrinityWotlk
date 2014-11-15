c3cec364e6b6bd108fd70ec3108234fe26fd0eb4
Grumbo <blackwolfsden@hotmail.com>
2014-11-15 13:49:15 -0700

    Update World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1046,7 +1046,7 @@ void World::LoadConfigSettings(bool reload)
     m_int_configs[CONFIG_ARENA_START_MATCHMAKER_RATING]              = sConfigMgr->GetIntDefault ("Arena.ArenaStartMatchmakerRating", 1500);
     m_bool_configs[CONFIG_ARENA_SEASON_IN_PROGRESS]                  = sConfigMgr->GetBoolDefault("Arena.ArenaSeason.InProgress", true);
     m_bool_configs[CONFIG_ARENA_LOG_EXTENDED_INFO]                   = sConfigMgr->GetBoolDefault("ArenaLog.ExtendedInfo", false);
-    m_int_configs[CONFIG_VIP_MAX]                                    = sConfigMgr->GetIntDefault("VIP.Max", 6);
+    m_int_configs[CONFIG_VIP_MAX]                                    = sConfigMgr->GetIntDefault("VIP.MAX", 6);
     
     m_bool_configs[CONFIG_OFFHAND_CHECK_AT_SPELL_UNLEARN]            = sConfigMgr->GetBoolDefault("OffhandCheckAtSpellUnlearn", true);
 
