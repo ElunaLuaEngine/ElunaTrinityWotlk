1558fa62b660d70985464789db9b80f7a0332451
Clement Denis <le.mikmac@gmail.com>
2020-10-27 00:10:44 +0100

    re-implement OnCommand Hook after ChatCommand.cpp refactor

--- src/server/game/Chat/ChatCommands/ChatCommand.cpp
+++ src/server/game/Chat/ChatCommands/ChatCommand.cpp
@@ -25,6 +25,9 @@
 #include "Map.h"
 #include "Player.h"
 #include "ScriptMgr.h"
+#ifdef ELUNA
+#include "LuaEngine.h"
+#endif
 #include "WorldSession.h"
 
 using ChatSubCommandMap = std::map<std::string_view, Trinity::Impl::ChatCommands::ChatCommandNode, StringCompareLessI_T>;
@@ -298,11 +301,20 @@ namespace Trinity::Impl::ChatCommands
         }
         else if (!handler.HasSentErrorMessage())
         { /* invocation failed, we should show usage */
+#ifdef ELUNA
+            if (!sEluna->OnCommand(handler.IsConsole() ? NULL : handler.GetSession()->GetPlayer(), std::string(cmdStr).c_str()))
+                return true;
+#endif
             cmd->SendCommandHelp(handler);
             handler.SetSentErrorMessage(true);
         }
         return true;
     }
+
+#ifdef ELUNA
+    if (!sEluna->OnCommand(handler.IsConsole() ? NULL : handler.GetSession()->GetPlayer(), std::string(cmdStr).c_str()))
+        return true;
+#endif
 
     return false;
 }
