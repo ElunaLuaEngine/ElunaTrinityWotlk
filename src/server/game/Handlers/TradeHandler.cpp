08e6e2d3b3bf62439ab2e0118b96ec0e9edb238c
Foereaper <foereaper@elunatech.com>
2023-10-13 22:09:35 +0200

    Added Trade Init and Accept hooks

--- src/server/game/Handlers/TradeHandler.cpp
+++ src/server/game/Handlers/TradeHandler.cpp
@@ -30,6 +30,9 @@
 #include "TradeData.h"
 #include "World.h"
 #include "WorldPacket.h"
+#ifdef ELUNA
+#include "LuaEngine.h"
+#endif
 
 void WorldSession::SendTradeStatus(TradeStatusInfo const& info)
 {
@@ -351,6 +354,17 @@ void WorldSession::HandleAcceptTradeOpcode(WorldPacket& /*recvPacket*/)
             //}
         }
     }
+
+#ifdef ELUNA
+    if (!sEluna->OnTradeAccept(_player, trader))
+    {
+        info.Status = TRADE_STATUS_CLOSE_WINDOW;
+        info.Result = EQUIP_ERR_CANT_DO_RIGHT_NOW;
+        SendTradeStatus(info);
+        my_trade->SetAccepted(false, true);
+        return;
+    }
+#endif
 
     if (his_trade->IsAccepted())
     {
@@ -698,6 +712,15 @@ void WorldSession::HandleInitiateTradeOpcode(WorldPacket& recvPacket)
         SendTradeStatus(info);
         return;
     }
+
+#ifdef ELUNA
+    if (!sEluna->OnTradeInit(GetPlayer(), pOther))
+    {
+        info.Status = TRADE_STATUS_BUSY;
+        SendTradeStatus(info);
+        return;
+    }
+#endif
 
     // OK start trade
     _player->m_trade = new TradeData(_player, pOther);
