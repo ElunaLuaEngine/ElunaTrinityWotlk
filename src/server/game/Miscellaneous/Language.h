8fd14cf4877321da2bfdf7d1fdb475ef2e4f098b
Krutok <47283866+Krutok@users.noreply.github.com>
2025-07-03 09:53:02 +0200

    Update Language.h

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -1243,7 +1243,7 @@ enum TrinityStrings
     LANG_SHUTDOWN_CANCELLED                       = 11018,
     LANG_YOU_CHANGE_POWER                         = 11019, // master branch ONLY
     LANG_YOUR_POWER_CHANGED                       = 11020, // master branch ONLY
-    LANG_INVALID_POWER_NAME                       = 11021  // master branch ONLY
+    LANG_INVALID_POWER_NAME                       = 11021,  // master branch ONLY
     LANG_BREWFEST_DARK_IRON_EVENT_ANNOUNCE        = 15000,
     LANG_BREWFEST_TIME_LEFT_ANNOUNCE              = 15001
 };
